c9b1bf3e547d2c51cbea5e6d3410d7f19a2cfec2
omid <omiiidh@gmail.com>
2022-04-01 20:29:29 +0430

    test2

---
+++ S06L13/src/Enc.java
@@ -0,0 +1,46 @@
+class demo{
+	private String name;
+	private int id;
+	
+	
+	public void SetData(String name,int id) {
+		this.id =  id;
+		this.name = name;
+		//System.out.println("Name : "+ this.name);
+		//System.out.println("id : "+ this.id);
+
+		
+	}
+	
+	
+	public int getId() {
+		return id;
+	}
+	public String getname() {
+		return name;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+public class Enc {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		demo user = new demo();
+		user.SetData("omid", 18);
+		System.out.println("ID: "+user.getId());
+		System.out.println("Name: "+user.getname());
+
+	}
+
+}
